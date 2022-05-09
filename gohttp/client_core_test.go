06a5587c56957fa40b5591c2ebdc6a3542ae1ce9
Melvin van Gent <melvinvangent@gmail.com>
2022-05-09 11:57:01 +0200

    Adding Tests for getRequestBody on the client

--- gohttp/client_core_test.go
+++ gohttp/client_core_test.go
@@ -37,3 +37,63 @@ func TestGetRequestHeaders(t *testing.T) {
 		t.Error("Invalid X-Request-Id received")
 	}
 }
+
+func TestGetRequestBodyNilBody(t *testing.T) {
+	// Initialization
+	client := httpClient{}
+
+	t.Run("noBodyNilResponse", func(t *testing.T) {
+		requestBody, err := client.getRequestBody("application/json", nil)
+
+		if err != nil {
+			t.Error("No error expected when passing a nil body")
+		}
+
+		if requestBody != nil {
+			t.Error("No body expected when passing a nil body")
+		}
+	})
+
+	t.Run("BodyWithJson", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+
+		body, err := client.getRequestBody("application/json", requestBody)
+
+		if err != nil {
+			t.Error("No error expected when marshalling a slice as json")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("Invalid json body when marshalling slice as json")
+		}
+	})
+
+	t.Run("BodyWithXml", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+
+		body, err := client.getRequestBody("application/xml", requestBody)
+
+		if err != nil {
+			t.Error("No error expected when marshalling a slice as xml")
+		}
+
+		if string(body) != `<string>one</string><string>two</string>` {
+			t.Error("Invalid xml body when marshalling slice as xml")
+		}
+	})
+
+	t.Run("BodyWithJsonAsDefault", func(t *testing.T) {
+		requestBody := []string{"one", "two"}
+
+		body, err := client.getRequestBody("", requestBody)
+
+		if err != nil {
+			t.Error("No error expected when marshalling a slice as json by default")
+		}
+
+		if string(body) != `["one","two"]` {
+			t.Error("Invalid json body when marshalling slice as json by default")
+		}
+	})
+
+}
