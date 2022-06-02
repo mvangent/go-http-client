ea94554149491603807160520f5281c32ac9ddc9
Melvin van Gent <melvinvangent@gmail.com>
2022-06-02 14:40:55 +0200

    2/3 coverage of mock server tests implemented

---
+++ gohttp_mock/mock_server_test.go
@@ -0,0 +1,71 @@
+package gohttp_mock
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSuccesfulMockedScenario(t *testing.T) {
+	assert.NotNil(t, MockupServer)
+
+	MockupServer.Start()
+
+	assert.Equal(t, true, MockupServer.IsEnabled())
+
+	var (
+		method       = "POST"
+		url          = "https://bahn.de/search"
+		requestBody  = `{"user":"vpofe","destination":"Amsterdam", "departure": "Berlin", "datetime":"22052022-07H00M"}`
+		statusCode   = 201
+		responseBody = `{results: [IC2201, [ICE78, IC34]]}`
+	)
+
+	testMock := Mock{
+		Method:      method,
+		Url:         url,
+		RequestBody: requestBody,
+
+		ResponseStatusCode: statusCode,
+		ResponseBody:       responseBody,
+	}
+
+	MockupServer.AddMock(testMock)
+
+	client := MockupServer.GetClient()
+
+	assert.NotNil(t, client)
+
+	type TravelInquiry struct {
+		User        string `json:"user"`
+		Destination string `json:"destination"`
+		Departure   string `json:"departure"`
+		Datetime    string `json:"datetime"`
+	}
+
+	travelInquiry := TravelInquiry{
+		User:        "vpofe",
+		Destination: "Amsterdam",
+		Departure:   "Berlin",
+		Datetime:    "22052022-07H00M",
+	}
+
+	buf, err := json.Marshal(travelInquiry)
+
+	assert.Nil(t, err)
+
+	request, err := http.NewRequest(method, url, bytes.NewBuffer(buf))
+
+	assert.Nil(t, err)
+	assert.NotNil(t, request)
+
+	response, err := client.Do(request)
+
+	assert.Nil(t, err)
+
+	assert.Equal(t, statusCode, response.StatusCode)
+	// assert.Equal(t, responseBody, response.Body)
+}
